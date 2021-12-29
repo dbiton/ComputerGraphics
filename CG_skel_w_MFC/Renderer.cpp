c97e7366d5c45fffb3e6b0692745adad8ea3a07a
Dvir-David Biton <dbiton@campus.technion.ac.il>
2021-12-29 21:00:41 +0200

    bloom

--- CG_skel_w_MFC/Renderer.cpp
+++ CG_skel_w_MFC/Renderer.cpp
@@ -573,7 +573,7 @@ void Renderer::applyEffects() {
                     for (int i = 0; i < m_spreadBloom * 2 + 1; i++) {
                         int x_curr = x + i - m_spreadBloom;
                         if (x_curr > 0 && x_curr < m_width) {
-                            m_bloomBuffer[INDEX(m_width, x, y, c)] = m_bloomBuffer[INDEX(m_width, x_curr, y, c)] * m_weightsBloom[i];
+                            m_bloomBuffer[INDEX(m_width, x_curr, y, c)] += m_bloomBuffer[INDEX(m_width, x, y, c)] * m_weightsBloom[i];
                         }
                     }
                 }
@@ -586,7 +586,7 @@ void Renderer::applyEffects() {
                     for (int i = 0; i < m_spreadBloom * 2 + 1; i++) {
                         int y_curr = y + i - m_spreadBloom;
                         if (y_curr > 0 && y_curr < m_height) {
-                            m_bloomBuffer[INDEX(m_width, x, y, c)] = m_bloomBuffer[INDEX(m_width, x, y_curr, c)] * m_weightsBloom[i];
+                            m_bloomBuffer[INDEX(m_width, x, y_curr, c)] += m_bloomBuffer[INDEX(m_width, x, y, c)] * m_weightsBloom[i];
                         }
                     }
                 }
@@ -613,7 +613,8 @@ void Renderer::blendBloomBuffer() {
     for (int x = 0; x < m_width; x++) {
         for (int y = 0; y < m_height; y++) {
             for (int c = 0; c < 3; c++) {
-                m_outBuffer[INDEX(m_width, x, y, c)] = min(1.f, m_outBuffer[INDEX(m_width, x, y, c)] + m_bloomBuffer[INDEX(m_width, x, y, c)]);
+                float v = m_outBuffer[INDEX(m_width, x, y, c)] + m_bloomBuffer[INDEX(m_width, x, y, c)];
+                m_outBuffer[INDEX(m_width, x, y, c)] = 1.f - 1 / (1 + v); // fit between 0 and 1
             }
         }
     }
